e7f8bcdd601cc6ddb6d8222cdd87e59fdbf66b58
github2k15 <github2k15@sharadb.net>
2015-09-14 01:52:28 +0000

    Adding bin_defs.txt reader

---
+++ code/bbb_common/bin_reader.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <assert.h>
+
+#include "bin_reader.h"
+
+
+static int
+next_param( char *buf, unsigned int start )
+{
+#ifdef DEBUG
+   printf( "> %d %d %c\n", start, buf[start], buf[start] );
+#endif
+
+   while ( buf[start] ) {
+      if ( buf[start] != ' ' && buf[start] != '\t' ) {
+         return start;
+      }
+      start++;
+   }
+
+   printf( "Can't parse %s", buf );
+   assert( 0 );
+
+   return -1;
+}
+
+
+static int
+next_blank( char *buf, unsigned int start )
+{
+   while ( buf[start] ) {
+      if ( buf[start] == ' ' || buf[start] == '\t' ) {
+         return start;
+      }
+      start++;
+   }
+
+   printf( "Can't parse %s", buf );
+   assert( 0 );
+
+   return -1;
+}
+
+
+
+unsigned int
+bins_load( char *filename, bin *bins )
+{
+   FILE *f;
+   unsigned int nr_bins = 0;
+   char buf[1024];
+   int offset;
+
+   f = fopen( filename, "r" );
+   assert( f );
+
+   while ( fgets( buf, sizeof( buf ), f ) && !feof( f ) ) {
+      if ( buf[0] == '#' || strlen( buf ) < 7 ) {
+         continue;
+      }
+
+      sscanf( buf, "%s ", bins[nr_bins].name );
+
+      offset = next_param( buf, strlen( bins[nr_bins].name ) );
+      bins[nr_bins].freq_low = atoi( &buf[offset] ); 
+
+      offset = next_param( buf, next_blank( buf, offset ) );
+      bins[nr_bins].freq_high = atoi( &buf[offset] );
+
+      offset = next_param( buf, next_blank( buf, offset ) );
+      bins[nr_bins].threshold = atoi( &buf[offset] );
+
+#ifdef DEBUG
+      printf( "-%s-%d-%d-%d-\n", bins[nr_bins].name,
+              bins[nr_bins].freq_low, bins[nr_bins].freq_high,
+              bins[nr_bins].threshold );
+#endif
+
+      nr_bins++;
+   }
+
+   fclose( f );
+
+   return nr_bins;
+}
+
---
+++ code/bbb_common/bin_reader.h
@@ -0,0 +1,10 @@
+#ifndef __BIN_READER_H
+#define __BIN_READER_H
+
+#include "pb_bins.h"
+
+// returns the number of bins loaded from the file
+unsigned int bins_load( char *filename, bin *bins );
+
+#endif // __BIN_READER_H
+

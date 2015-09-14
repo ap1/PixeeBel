ca1833e3cc2cb0fb0edc59d9c58c9d0631028e37
github2k15 <github2k15@sharadb.net>
2015-09-14 09:00:43 +0000

    stretch visualization working

--- code/bbb_visualize/pb_bin_reader.py
+++ code/bbb_visualize/pb_bin_reader.py
@@ -15,7 +15,12 @@ def load( filename ):
    for line in [ l for l in lines if l[0] != "#" ]:
       m = binDescRe.match( line )
       if m:
-         bins[ count ] = m.groupdict()
+         bins[count] = m.groupdict()
+         bins[count]["freq_low"] = int( bins[count]["freq_low"] )
+         bins[count]["freq_high"] = int( bins[count]["freq_high"] )
+         bins[count]["threshold"] = float( bins[count]["threshold"] )
+
+         assert( bins[count]["freq_low"] < bins[count]["freq_high"] )
          count += 1
 
    return bins
--- code/bbb_visualize/screen.py
+++ code/bbb_visualize/screen.py
@@ -29,6 +29,9 @@ bins = None
 display = None
 
 
+min_bin_freq = MAX_FREQ
+max_bin_freq = 0
+
 
 class Display( object ):
 
@@ -44,7 +47,8 @@ class Display( object ):
       self.draw = ImageDraw.Draw( self.image )
 
       self.barLength = min( LCD.LCDHEIGHT, LCD.LCDHEIGHT )
-      self.k = self.barLength / math.log( MAX_FREQ )
+      self.k_flat = self.barLength / math.log( MAX_FREQ )
+      self.k_stretch = self.barLength / math.log( max_bin_freq / min_bin_freq )
 
    def clear( self ):
       self.disp.clear()
@@ -60,18 +64,28 @@ class Display( object ):
       freq_low = bins[binId]['freq_low']
       freq_high = bins[binId]['freq_high']
 
+      def COORD_FLAT( freq ):
+         return math.log( freq ) * self.k_flat
+
+      def COORD_STRETCH( freq ):
+         return math.log( freq / min_bin_freq ) * self.k_stretch
+
+      def COORD( freq ):
+         return COORD_STRETCH( freq )
+
       if edge in "fb":
          offset_x = ( LCD.LCDWIDTH - self.barLength ) / 2
 
-         x1 = offset_x + math.log( float( freq_low ) ) * self.k
-         x2 = offset_x + math.log( float( freq_high ) ) * self.k
+         x1 = offset_x + COORD( freq_low )
+         x2 = offset_x + COORD( freq_high )
 
          y1 = y2 = binId if edge == "f" else ( LCD.LCDHEIGHT - binId - 1 )
       else:
          offset_y = ( LCD.LCDHEIGHT - self.barLength ) / 2
 
-         y1 = offset_y + math.log( float( freq_low ) ) * self.k
-         y2 = offset_y + math.log( float( freq_high ) ) * self.k
+         y1 = offset_y + COORD( freq_low )
+         y2 = offset_y + COORD( freq_high )
+
          x1 = x2 = binId if edge == "l" else ( LCD.LCDWIDTH - binId - 1 )
 
       return ( int( x1 ), int( y1 ), math.ceil( x2 ), math.ceil( y2 ) )
@@ -88,10 +102,14 @@ class Display( object ):
 
 
 def init():
-   global bins, display
+   global bins, display, min_bin_freq, max_bin_freq
 
    bins = pb_bin_reader.load( "../bbb_common/bin_defs.txt" )
    for binValue in bins.values(): print binValue
+
+   for binValue in bins.values():
+      min_bin_freq = min( min_bin_freq, binValue["freq_low"] )
+      max_bin_freq = max( max_bin_freq, binValue["freq_high"] )
 
    display = Display()
 
